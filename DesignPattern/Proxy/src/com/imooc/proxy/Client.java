8f87f050acee266e72470d1974dd3232eb33655a
Muscleape529 <muscleape529@gmail.com>
2018-04-30 16:41:49 +0800

    测试类中，通过实例化Car2调用对应的方法测试

--- DesignPattern/Proxy/src/com/imooc/proxy/Client.java
+++ DesignPattern/Proxy/src/com/imooc/proxy/Client.java
@@ -5,7 +5,9 @@ package com.imooc.proxy;
  */
 public class Client {
 	public static void main(String[] args) {
-		Car car = new Car();
-		car.move();
+		// Car car = new Car();
+		// car.move();
+		Moveable m = new Car2();
+		m.move();
 	}
 }
