0d75cf67c1836488bfd5fbeb529ed556273b0714
Muscleape529 <muscleape529@gmail.com>
2018-04-30 16:35:20 +0800

    汽车的行驶方法中添加行驶时间记录功能，记录汽车行驶的开始时间、结束时间及行驶的时长

--- DesignPattern/Proxy/src/com/imooc/proxy/Car.java
+++ DesignPattern/Proxy/src/com/imooc/proxy/Car.java
@@ -6,12 +6,18 @@ public class Car implements Moveable {
 
 	@Override
 	public void move() {
+		// 在车中添加功能，记录行驶时间，即记录move方法执行的开似乎时间和结束时间
+		long startTime = System.currentTimeMillis();
+		System.out.println("汽车开始行驶。。。");
 		// 实现开车
 		try {
 			Thread.sleep(new Random().nextInt(1000));
+			System.out.println("汽车行驶中。。。");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("汽车结束行驶。。。汽车行驶时间：" + (endTime - startTime) + "毫秒");
 	}
 
 }
---
+++ DesignPattern/Proxy/src/com/imooc/proxy/Client.java
@@ -0,0 +1,11 @@
+package com.imooc.proxy;
+
+/**
+ * 测试类
+ */
+public class Client {
+	public static void main(String[] args) {
+		Car car = new Car();
+		car.move();
+	}
+}
